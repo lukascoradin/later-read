35819a3a51adac10116b216e7018d51785462e18
Lucas Coradin <lukascoradin@hotmail.com>
2025-07-24 16:55:12 -0300

    fix: identation

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
   devise_for :users
-  resources :articles, only: [:index]
+  resources :articles, only: [ :index ]
 end
